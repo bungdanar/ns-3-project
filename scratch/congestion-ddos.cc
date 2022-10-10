5725c6d7b4588595ba95a8fce3a53f9be8140cd6
bungdanar <danar.g.putera@gmail.com>
2022-10-10 11:07:55 +0700

    chore: Fix project topology

--- scratch/congestion-ddos.cc
+++ scratch/congestion-ddos.cc
@@ -4,75 +4,78 @@
 #include "ns3/yans-wifi-helper.h"
 #include "ns3/ssid.h"
 #include "ns3/mobility-module.h"
+#include "ns3/internet-module.h"
 
 using namespace ns3;
 
 int
 main (int argc, char *argv[])
 {
-  /*
-  Set first topology/server
-  */
+  auto globalDataRate = StringValue ("100Mbps");
+  auto globalDelay = StringValue ("1ms");
 
-  // Should extract node count magic number
-  NodeContainer p2pServerNodes;
-  p2pServerNodes.Create (2);
+  // Routers
+  NodeContainer routerNodes;
+  routerNodes.Create (3);
 
-  // Should extract magic numbers and set them as project parameters
-  PointToPointHelper p2pHelper;
-  p2pHelper.SetDeviceAttribute ("DataRate", StringValue ("5mbps"));
-  p2pHelper.SetChannelAttribute ("Delay", StringValue ("2ms"));
+  PointToPointHelper ppR01, ppR02;
+  ppR01.SetDeviceAttribute ("DataRate", globalDataRate);
+  ppR01.SetChannelAttribute ("Delay", globalDelay);
 
-  // Install net device to server nodes
-  NetDeviceContainer p2pServerDevices;
-  p2pServerDevices = p2pHelper.Install (p2pServerNodes);
+  ppR02.SetDeviceAttribute ("DataRate", globalDataRate);
+  ppR02.SetChannelAttribute ("Delay", globalDelay);
 
-  /*
-  Set second topology/wired nodes using csma/bus topology
-  */
+  NetDeviceContainer dR01, dR02;
+  dR01 = ppR01.Install (routerNodes.Get (0), routerNodes.Get (1));
+  dR02 = ppR02.Install (routerNodes.Get (0), routerNodes.Get (2));
 
-  // Should extract node count magic number and set it as project parameters
-  NodeContainer csmaWiredNodes;
-  csmaWiredNodes.Add (p2pServerNodes.Get (1));
-  csmaWiredNodes.Create (11);
+  // Server
+  NodeContainer serverNode;
+  serverNode.Create (1);
 
-  // Should extract magic numbers and set them as project parameters
-  CsmaHelper csmaHelper;
-  csmaHelper.SetDeviceAttribute ("DataRate", StringValue ("100mbps"));
-  csmaHelper.SetChannelAttribute ("Delay", TimeValue (NanoSeconds (6560)));
+  PointToPointHelper ppS0R0;
+  ppS0R0.SetDeviceAttribute ("DataRate", globalDataRate);
+  ppS0R0.SetChannelAttribute ("Delay", globalDelay);
 
-  // Install net device to wired nodes
-  NetDeviceContainer csmaWiredDevices;
-  csmaWiredDevices = csmaHelper.Install (csmaWiredNodes);
+  NetDeviceContainer dS0R0;
+  dS0R0 = ppS0R0.Install (serverNode.Get (0), routerNodes.Get (0));
 
-  /*
-  Set third topology/wireless nodes using wireless topology
-  */
+  // Wired clients
+  NodeContainer wiredClientNodes;
+  wiredClientNodes.Add (routerNodes.Get (1));
+  wiredClientNodes.Create (10);
 
-  // Should extract node count magic number and set it as project parameters
+  CsmaHelper csma;
+  csma.SetDeviceAttribute ("DataRate", globalDataRate);
+  csma.SetChannelAttribute ("Delay", globalDelay);
+
+  NetDeviceContainer csmaDevices;
+  csmaDevices = csma.Install (wiredClientNodes);
+
+  // Wireless clients
   NodeContainer wifiStaNodes;
-  wifiStaNodes.Create (11);
-  NodeContainer wifiApNode = p2pServerNodes.Get (1);
+  wifiStaNodes.Create (10);
+  NodeContainer wifiApNode = routerNodes.Get (2);
 
-  YansWifiChannelHelper wifiChannelHelper = YansWifiChannelHelper::Default ();
-  YansWifiPhyHelper wifiPhysicalHelper;
-  wifiPhysicalHelper.SetChannel (wifiChannelHelper.Create ());
+  YansWifiChannelHelper wifiChannel = YansWifiChannelHelper::Default ();
+  YansWifiPhyHelper wifiPhysical;
+  wifiPhysical.SetChannel (wifiChannel.Create ());
 
-  WifiMacHelper wifiMacHelper;
+  WifiMacHelper wifiMac;
   Ssid wifiSsid = Ssid ("ns-3-ssid");
 
-  WifiHelper wifiHelper;
+  WifiHelper wifi;
 
   // Install net device to wifi station nodes
   NetDeviceContainer wifiStaDevices;
-  wifiMacHelper.SetType ("ns3::StaWifiMac", "Ssid", SsidValue (wifiSsid), "ActiveProbing",
-                         BooleanValue (false));
-  wifiStaDevices = wifiHelper.Install (wifiPhysicalHelper, wifiMacHelper, wifiStaNodes);
+  wifiMac.SetType ("ns3::StaWifiMac", "Ssid", SsidValue (wifiSsid), "ActiveProbing",
+                   BooleanValue (false));
+  wifiStaDevices = wifi.Install (wifiPhysical, wifiMac, wifiStaNodes);
 
   // Install net device to wifi access point node
   NetDeviceContainer wifiApDevices;
-  wifiMacHelper.SetType ("ns3::ApWifiMac", "Ssid", SsidValue (wifiSsid));
-  wifiApDevices = wifiHelper.Install (wifiPhysicalHelper, wifiMacHelper, wifiApNode);
+  wifiMac.SetType ("ns3::ApWifiMac", "Ssid", SsidValue (wifiSsid));
+  wifiApDevices = wifi.Install (wifiPhysical, wifiMac, wifiApNode);
 
   // Set wifi mobility
   MobilityHelper mobilityHelper;
@@ -88,6 +91,65 @@ main (int argc, char *argv[])
 
   mobilityHelper.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
   mobilityHelper.Install (wifiApNode);
+
+  /*
+    Set bots topology using simple point-to-point (for now)
+  */
+  int botNumbers = 50;
+  NodeContainer botNodes;
+  botNodes.Create (botNumbers);
+
+  PointToPointHelper p2pBot;
+  p2pBot.SetDeviceAttribute ("DataRate", globalDataRate);
+  p2pBot.SetChannelAttribute ("Delay", globalDelay);
+
+  // Install net device to bot nodes
+  NetDeviceContainer botDevices[botNumbers];
+  for (size_t i = 0; i < botNumbers; i++)
+    {
+      botDevices[i] = p2pBot.Install (routerNodes.Get (0), botNodes.Get (i));
+    }
+
+  /*
+    Install Internet Stack to all nodes
+  */
+  InternetStackHelper stack;
+  stack.Install (routerNodes);
+  stack.Install (serverNode);
+  stack.Install (wiredClientNodes);
+  stack.Install (wifiApNode);
+  stack.Install (wifiStaNodes);
+  stack.Install (botNodes);
+
+  /*
+    Set IPv4 address
+  */
+  Ipv4AddressHelper address;
+
+  // For server network
+  address.SetBase ("10.1.1.0", "255.255.255.0");
+  Ipv4InterfaceContainer serverInterfaces;
+  serverInterfaces = address.Assign (dS0R0);
+
+  // For R01
+  address.SetBase ("10.1.2.0", "255.255.255.0");
+  Ipv4InterfaceContainer r01Interfaces;
+  r01Interfaces = address.Assign (dR01);
+
+  // For R02
+  address.SetBase ("10.1.3.0", "255.255.255.0");
+  Ipv4InterfaceContainer r02Interfaces;
+  r02Interfaces = address.Assign (dR02);
+
+  // For wired network
+  address.SetBase ("10.1.4.0", "255.255.255.0");
+  Ipv4InterfaceContainer wiredInterfaces;
+  wiredInterfaces = address.Assign (csmaDevices);
+
+  // For wireless network
+  address.SetBase ("10.1.5.0", "255.255.255.0");
+  address.Assign (wifiStaDevices);
+  address.Assign (wifiApDevices);
 
   return 0;
 }
